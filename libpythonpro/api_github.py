c60e29e6d097b4ef121259ae3f76a5c37c962fa3
hgf777-br <hgf777@gmail.com>
2022-02-07 17:16:10 -0300

    flake 8

--- libpythonpro/api_github.py
+++ libpythonpro/api_github.py
@@ -1,6 +1,7 @@
 import requests
 
-def buscar_avatar(usuario:str) -> str:
+
+def buscar_avatar(usuario: str) -> str:
     """
     Busca a imagem do avatar de um usuário no Github
 
@@ -11,10 +12,11 @@ def buscar_avatar(usuario:str) -> str:
         str: link do avatar
     """
     url = f"https://api.github.com/users/{usuario}"
-    
+
     resp = requests.get(url)
-    
+
     return resp.json()["avatar_url"]
+
 
 if __name__ == "__main__":
     print(buscar_avatar("hgf777-br"))
